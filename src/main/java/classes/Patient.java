9230c3e5d1ffece674a2649aceb4b30c7c86ea6a
amineaith3 <amineaithamma2004@gmail.com>
2024-04-13 21:58:53 +0000

    bdlnaDBModelPatient

--- src/main/java/classes/Patient.java
+++ src/main/java/classes/Patient.java
@@ -12,4 +12,88 @@ public class Patient {
     private StringBuffer address;
     private StringBuffer note;
     private StringBuffer mutuelle;
+
+    public Patient(String cin, String prenom, String nom, StringBuffer tel, boolean sexe, Date dateNaissance, StringBuffer address, StringBuffer note, StringBuffer mutuelle) {
+        this.cin = cin;
+        this.prenom = prenom;
+        this.nom = nom;
+        this.tel = tel;
+        this.sexe = sexe;
+        this.dateNaissance = dateNaissance;
+        this.address = address;
+        this.note = note;
+        this.mutuelle = mutuelle;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public StringBuffer getTel() {
+        return tel;
+    }
+
+    public void setTel(StringBuffer tel) {
+        this.tel = tel;
+    }
+
+    public boolean isSexe() {
+        return sexe;
+    }
+
+    public void setSexe(boolean sexe) {
+        this.sexe = sexe;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public StringBuffer getAddress() {
+        return address;
+    }
+
+    public void setAddress(StringBuffer address) {
+        this.address = address;
+    }
+
+    public StringBuffer getNote() {
+        return note;
+    }
+
+    public void setNote(StringBuffer note) {
+        this.note = note;
+    }
+
+    public StringBuffer getMutuelle() {
+        return mutuelle;
+    }
+
+    public void setMutuelle(StringBuffer mutuelle) {
+        this.mutuelle = mutuelle;
+    }
 }
