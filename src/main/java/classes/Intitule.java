5ae8cd77b5c20577fc7c84897c0e3d28449c008a
amineaith3 <amineaithamma2004@gmail.com>
2024-04-16 23:57:14 +0100

    test test

---
+++ src/main/java/classes/Intitule.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class Intitule {
+    String data;
+
+    public Intitule(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
---
+++ src/main/java/classes/Prescription.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class Prescription {
+    String data;
+
+    public Prescription(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
